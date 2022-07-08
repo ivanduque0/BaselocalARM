6625ad1ee40bb0f97b98e63d5437ddfffded6f6a
ivanduque0 <98676748+ivanduque0@users.noreply.github.com>
2022-07-08 00:28:55 -0400

    Update codigo.py
    ahora si el bot deja de hacer pooling por alguna razon, se vuelve a intentar conectar

--- botelegram/codigo.py
+++ botelegram/codigo.py
@@ -318,9 +318,8 @@ while True:
             port=os.environ.get("PORT")
         )
         cursor = conn.cursor()
-        bot.infinity_polling()
+        bot.polling(skip_pending=True)
         bot.stop_polling()
-        bot = telebot.TeleBot(token, parse_mode=None)
         
 
 
